4e7870ddf802dbe021c04273abd6d45d1c7aef83
techtuh <139290836+techtuh@users.noreply.github.com>
2024-09-23 10:53:15 -0400

    about me

---
+++ BusinessWebsite/file.js
@@ -0,0 +1,12 @@
+alert ("hello how are you");
+document.write("this my js");
+
+var name = window.prompt("enter your name: ");
+alert("your name is "+ name);
+var age = 16;
+var grade   = 10;
+var gpa = 4.3;
+
+document.write(name + "<br>");
+
+document.write(age);
